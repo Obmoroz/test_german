6779edb93681aebfcc704828a4738fa53f18b9bf
Evgeniy <Obmorozz@gmail.com>
2018-10-12 11:15:05 +0300

    first test

--- public/php/worker.php
+++ public/php/worker.php
@@ -1,7 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 12.10.18
- * Time: 10:55
- */
+$worker = new GearmanWorker();
+$worker->addServer();
+
+$worker->addFunction('sendmail', 'send_mail');
+
+while (1)
+{
+    $worker->work();
+    if ($worker->returnCode() != GEARMAN_SUCCESS) break;
+}
+
+function send_mail($job)
+{
+    $workload = $job->workload();
+    $data = json_decode($workload, true);
+    var_dump($data);
+    //mail($data['to'], $data['subject'], $data['body']);
+}
